f69a6c240cba166a798b889c93a01f6c47dec174
CoolTurtle <galbanmark96@gmail.com>
2025-10-12 12:30:05 +0800

    Update user_database.php

--- ValCommuters/user_database.php
+++ ValCommuters/user_database.php
@@ -1,16 +1,1 @@
- <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "valcommutersdb";
 
-try {
-	$conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
-	
-	$conn-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	echo "Connected Successfully";
-} catch (PDOException $e) {
-	echo "Connection Failed: " . $e->getMessage();
-}
-
-?>
